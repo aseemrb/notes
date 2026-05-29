c26c19d7ed156a9843020e1996311b7e0b1e2d27
Aseem Baranwal <aseemrb@gmail.com>
2026-05-29 01:00:29 -0400

    linear algebra: merge Least Squares and Computations into Matrix Computations

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,8 +37,7 @@ export default defineConfig({
 					items: [
 						{ slug: 'linear-algebra', label: 'Introduction' },
 						{ label: 'Fundamentals', collapsed: true, items: [{ autogenerate: { directory: 'linear-algebra/fundamentals', collapsed: true } }] },
-						{ label: 'Least Squares', collapsed: true, items: [{ autogenerate: { directory: 'linear-algebra/least-squares', collapsed: true } }] },
-						{ label: 'Computations', collapsed: true, items: [{ autogenerate: { directory: 'linear-algebra/computations', collapsed: true } }] },
+						{ label: 'Matrix Computations', collapsed: true, items: [{ autogenerate: { directory: 'linear-algebra/matrix-computations', collapsed: true } }] },
 						{ label: 'Optimization', collapsed: true, items: [{ autogenerate: { directory: 'linear-algebra/optimization', collapsed: true } }] },
 						{ label: 'Special Matrices', collapsed: true, items: [{ autogenerate: { directory: 'linear-algebra/special-matrices', collapsed: true } }] },
 					],
